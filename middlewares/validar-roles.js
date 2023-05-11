ca5af3a80313bc220dec90b3229086531e5d7f46
Asly Brillit Cantillo Barrios <asly.cantillo@ludycom.com>
2023-05-11 18:05:04 -0500

    token deletes

--- middlewares/validar-roles.js
+++ middlewares/validar-roles.js
@@ -1,16 +1,16 @@
 const { response } = require("express")
 
 
-const esAdminRole = (req, res = response, next) =>{
+const esAdminRole = (req, res = response, next) => {
 
-    if(!req.usuario){
-   return req.status(500).json({
-    msg: 'Se requere verificar el role sin validar el token primero'
-   });
+    if (!req.usuario) {
+        return res.status(500).json({
+            msg: 'Se requere verificar el role sin validar el token primero'
+        });
     }
 
-    const {rol, nombre} = req.usuario;
-    if(rol !== 'ADMIN_ROLE'){
+    const { rol, nombre } = req.usuario;
+    if (rol !== 'ADMIN_ROLE') {
         return res.status(401).json({
             msg: `${nombre} no es administrador - No puede hacer esto`
         });
@@ -18,6 +18,26 @@ const esAdminRole = (req, res = response, next) =>{
     next();
 }
 
+const tieneRole = (...roles) => {
+    return (req, res = response, next) => {
+        if (!req.usuario) {
+            return res.status(500).json({
+                msg: 'Se requere verificar el role sin validar el token primero'
+            });
+        }
+
+        if (roles.includes(req.usuario.rol)) {
+            return res.status(401).json({
+                msg: `El servicio requiere uno de estos roles ${roles}`
+            });
+        }
+
+        next();
+
+    }
+}
+
 module.exports = {
-    esAdminRole
+    esAdminRole,
+    tieneRole
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { esAdminRole } = require('../middlewares/validar-roles');
+const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
 
 
 const {esRoleValido, emailExiste, existeUsuarioPorId} = require('../helpers/db-validators');
@@ -37,8 +37,9 @@ router.post('/', [
 ], usuariosPost);
 
 router.delete('/:id', [
-  esAdminRole,
   validarJWT,
+  // esAdminRole,
+  tieneRole('ADMIN_ROLE', 'USER_ROLE'),
   check('id', 'No es un ID valido').isMongoId(),
   check('id').custom(existeUsuarioPorId),
   validarCampos
